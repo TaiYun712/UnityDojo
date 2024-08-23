80af2870ff5542ce0d25ee2df30534d6d84868df
TaiYun <wendy0712000@gmail.com>
2024-08-23 16:01:02 +0800

    我在start寫了一個開始

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("開始");
     }
 
     // Update is called once per frame
